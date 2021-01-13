fcee881da20dde6408df79426cea17b04a7937dc
Danilo <danilobenitez010@gmail.com>
2021-01-13 15:11:38 -0300

    add class

---
+++ BankService.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Newtonsoft.Json;
+using EmuladorCajero.DTO;
+using EmuladorCajero;
+
+namespace BankInterface
+{
+    internal class BankService : IDisposable
+    {
+        private string _host;
+        private string _token;
+        private int _tries;
+
+        public int Tries
+        {
+            get { return _tries; }
+            set { _tries = value; }
+        }
+        private static ManualResetEvent allDone = new ManualResetEvent(false);
+        const int BUFFER_SIZE = 1024;
+        const int MAX_TRIES = 3;
+
+        public BankService(string host, string user, string pass, ref ResponseDTO r)
+        {
+           // test(new Uri(host), user, pass);
+            _host = host;
+            Hashtable parametros = new Hashtable();
+            parametros.Add("username", user);
+            parametros.Add("password", pass);
+            Hashtable respuesta = Post("api/authenticate", parametros);
+             r = new ResponseDTO();
+            Mapper.MapObject(respuesta,r);
+            _token = Convert.ToString(respuesta["id_token"]);
+            r.status = !string.IsNullOrWhiteSpace(_token);
+            if (!r.status && string.IsNullOrWhiteSpace(r.message))
+            {
+                r.message = "Se produjo un error al obtener el token";
+            }
+
+        }
+        public Hashtable Get(string pRequestName, Dictionary<string, string> pParams = null)
+        {
+            _tries = 0;
+            return Call(pRequestName, "GET", pParams, null);
+        }
+        public Hashtable Post(string pRequestName, Hashtable pData)
+        {
+            _tries = 0;
+            return Call(pRequestName, "POST", null, pData);
+        }
+        public Hashtable Put(string pRequestName, Hashtable pData = null)
+        {
+            _tries = 0;
+            return Call(pRequestName, "PUT", null, pData);
+        }
+
+        private Hashtable Call(string pRequestName, string pMethod, Dictionary<string, string> pParams, Hashtable pData)
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.CreateDefault(GetUri(pParams, pRequestName)) as HttpWebRequest;
+                request.Method = pMethod;
+                request.ContentType = "application/json";
+                request.ProtocolVersion = HttpVersion.Version10;
+                request.KeepAlive = false;
+                System.Net.ServicePointManager.DefaultConnectionLimit = 200;
+
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls;
+                if (_token != null)
+                    request.Headers.Set(HttpRequestHeader.Authorization, "Bearer " + _token);
+                
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                byte[] data = null;
+                if (pData != null){
+                    data = encoder.GetBytes(JSON.JsonEncode(pData));
+                    request.ContentLength = data.Length;
+                }
+                else
+                    request.ContentLength = 0;
+                
+                //request.Timeout = 10000;
+                if (pData != null){
+                    request.AllowWriteStreamBuffering = false;
+                    using (var stream = request.GetRequestStream()){
+                        stream.Write(data, 0, data.Length);
+                    }
+                }
+                return GetResponse(request);
+            }
+            catch (Exception ex)
+            {
+                _tries++;
+
+                if (_tries <= 3)
+                {
+                    return Call(pRequestName, pMethod, pParams, pData);
+                }
+                else
+                {
+                    Hashtable error = new Hashtable();
+                    error.Add("status", false);
+                    error.Add("code", 400);
+                    error.Add("message", ex.Message);
+
+                    return error;
+                }
+            }
+        }
+
+        private Uri GetUri(Dictionary<string, string> pParams, string pRequestName)
+        {
+            string uri = string.Format("{0}{1}?", _host, pRequestName);
+            if (pParams != null && pParams.Count > 0)
+            {
+                foreach (var item in pParams)
+                {
+                    uri += string.Format("{0}={1}&", item.Key, item.Value != null ? System.Uri.EscapeDataString(item.Value) : item.Value);
+                }
+                uri = uri.Substring(0, uri.Length - 1);
+            }
+            return new Uri(uri);
+        }
+        private Hashtable GetResponse(HttpWebRequest request)
+        {
+            string outputDataStr = null;
+            Hashtable data = new Hashtable();
+            using (HttpWebResponse myWebResponse = request.GetResponse() as HttpWebResponse)
+            {
+                request.ServicePoint.MaxIdleTime = 0;
+               
+                using (Stream streamResponse = myWebResponse.GetResponseStream())
+                {
+                    using (StreamReader streamRead = new StreamReader(streamResponse))
+                    {
+                        Char[] readBuff = new Char[BUFFER_SIZE];
+                        int count = streamRead.Read(readBuff, 0, BUFFER_SIZE);
+
+                        while (count > 0)
+                        {
+                            outputDataStr += new String(readBuff, 0, count);
+                            count = streamRead.Read(readBuff, 0, BUFFER_SIZE);
+                        }
+                        if (!string.IsNullOrWhiteSpace(outputDataStr))
+                        {
+                            data = (Hashtable)JSON.JsonDecode(outputDataStr);
+                        }
+                        streamRead.Close();
+                        streamResponse.Close();
+                        myWebResponse.Close();
+                    }
+                }
+            }
+            return data;
+        }
+        
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ Mapper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Collections;
+using EmuladorCajero.DTO;
+
+namespace EmuladorCajero
+{
+    class Mapper
+    {
+        private static Assembly _thisAssemble;
+
+        private static Assembly GetAssembly()
+        {
+            if (_thisAssemble == null)
+            {
+                _thisAssemble = Assembly.GetExecutingAssembly();
+            }
+            return _thisAssemble;
+        }
+        private static void SetObjectProperty(string propertyName, object value, object obj)
+        {
+            PropertyInfo propertyInfo = obj.GetType().GetProperty(propertyName);
+            
+            if (propertyInfo != null)
+            {
+                if (value is Hashtable)
+                    MapObject((Hashtable)value, propertyInfo.GetValue(obj, null));
+                else if (value is ArrayList)
+                {
+                    ArrayList v = value as ArrayList;
+                    IList arr = (IList)propertyInfo.GetValue(obj, null);
+                    string arrName = Convert.ToString(arr);
+                    object newItem = GetAssembly().CreateInstance(arrName.Substring(arrName.IndexOf('[') + 1, arrName.Length - arrName.IndexOf('[') - 2));
+                    foreach (var item in v)
+                    {
+                        MapObject((Hashtable)item, newItem);
+                        arr.Add(newItem);
+                    }
+                }
+                else
+                    propertyInfo.SetValue(obj, value, null);
+            }
+        }
+        public static ResponseDTO MapResponse(object obj, Hashtable ht)
+        {
+            ResponseDTO res = new ResponseDTO() { responseData = obj};
+            MapObject(ht, res);
+            return res;
+        }
+
+        public static void MapObject(Hashtable ht, object res)
+        {
+            foreach (DictionaryEntry entry in ht)
+            {
+                if (res != null)
+                    SetObjectProperty(Convert.ToString(entry.Key), entry.Value, res);    
+            }
+        }
+
+    }
+}
